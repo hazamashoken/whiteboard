4b4548be189d59780d836f000b3d38436e49b09f
hazamashoken <earth78203@gmail.com>
2024-10-01 19:37:44 +0800

    fix build

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,10 @@
-import { NextRequest, NextResponse } from "next/server"
-import authConfig from "./auth.config"
-import NextAuth from "next-auth"
- 
+import { NextRequest, NextResponse } from "next/server";
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
 
-function checkPathRole(req: NextRequest, path: string, role: string) {
-    if (req.nextUrl.pathname.startsWith(path) && req.nextUrl.token?.role !== role) {
-        return new NextResponse("You are not authorized")
-    }
-}
- 
-const { auth } = NextAuth(authConfig)
-export default auth(async function middleware(req: NextRequest) {
-    // checkPathRole(req, )
-})
+const { auth } = NextAuth(authConfig);
+export default auth(async function middleware(req: NextRequest) {});
 
 export const config = {
-    matcher: [
-      '/((?!api|_next/static|_next/image|favicon.ico|home|test/*).*)',
-    ],
-  };
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|home|test/*).*)"],
+};
